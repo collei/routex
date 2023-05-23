8f6ac4d990f70f5104e5c3d4f558121a353071bf
Collei <collei@collei.com.br>
2023-05-23 14:43:04 -0300

    added syntax verificator methods

--- src/Routex/Parser/SyntaxAnalyzer.php
+++ src/Routex/Parser/SyntaxAnalyzer.php
@@ -3,7 +3,64 @@ namespace Routex\Parser;
 
 class SyntaxAnalyzer
 {
-	public static function linearize(array $tokens)
+	protected const ROUTEX_VALID_SEQUENCES = [
+		Tokenizer::ROUTEX_VERB => [
+			[ Tokenizer::ROUTEX_VERB, Tokenizer::ROUTEX_URI ],
+			[ Tokenizer::ROUTEX_URI, Tokenizer::ROUTEX_HANDLER ],
+			[ Tokenizer::ROUTEX_HANDLER, Tokenizer::ROUTEX_NAME ],
+			[ Tokenizer::ROUTEX_NAME, NULL ],
+		],
+		Tokenizer::ROUTEX_KEYWORD_WITH => [
+			[ Tokenizer::ROUTEX_KEYWORD_WITH, Tokenizer::ROUTEX_KEY_PREFIX ],
+			[ Tokenizer::ROUTEX_KEYWORD_WITH, Tokenizer::ROUTEX_KEY_NAME ],
+			[ Tokenizer::ROUTEX_KEYWORD_WITH, Tokenizer::ROUTEX_KEY_CONTROLLER ],
+			[ Tokenizer::ROUTEX_KEYWORD_WITH, Tokenizer::ROUTEX_KEY_MIDDLEWARE ],
+			[ Tokenizer::ROUTEX_KEY_PREFIX, Tokenizer::ROUTEX_URI ],
+			[ Tokenizer::ROUTEX_KEY_NAME, Tokenizer::ROUTEX_NAME ],
+			[ Tokenizer::ROUTEX_KEY_CONTROLLER, Tokenizer::ROUTEX_HANDLER ],
+			[ Tokenizer::ROUTEX_KEY_MIDDLEWARE, Tokenizer::ROUTEX_HANDLER ],
+			[ Tokenizer::ROUTEX_URI, Tokenizer::ROUTEX_KEY_PREFIX ],
+			[ Tokenizer::ROUTEX_URI, Tokenizer::ROUTEX_KEY_NAME ],
+			[ Tokenizer::ROUTEX_URI, Tokenizer::ROUTEX_KEY_CONTROLLER ],
+			[ Tokenizer::ROUTEX_URI, Tokenizer::ROUTEX_KEY_MIDDLEWARE ],
+			[ Tokenizer::ROUTEX_URI, NULL ],
+			[ Tokenizer::ROUTEX_NAME, Tokenizer::ROUTEX_KEY_PREFIX ],
+			[ Tokenizer::ROUTEX_NAME, Tokenizer::ROUTEX_KEY_NAME ],
+			[ Tokenizer::ROUTEX_NAME, Tokenizer::ROUTEX_KEY_CONTROLLER ],
+			[ Tokenizer::ROUTEX_NAME, Tokenizer::ROUTEX_KEY_MIDDLEWARE ],
+			[ Tokenizer::ROUTEX_NAME, NULL ],
+			[ Tokenizer::ROUTEX_HANDLER, Tokenizer::ROUTEX_KEY_PREFIX ],
+			[ Tokenizer::ROUTEX_HANDLER, Tokenizer::ROUTEX_KEY_NAME ],
+			[ Tokenizer::ROUTEX_HANDLER, Tokenizer::ROUTEX_KEY_CONTROLLER ],
+			[ Tokenizer::ROUTEX_HANDLER, Tokenizer::ROUTEX_KEY_MIDDLEWARE ],
+			[ Tokenizer::ROUTEX_HANDLER, NULL ],
+		],
+		Tokenizer::ROUTEX_KEYWORD_WITHOUT => [
+			[ Tokenizer::ROUTEX_KEYWORD_WITHOUT, Tokenizer::ROUTEX_KEY_PREFIX ],
+			[ Tokenizer::ROUTEX_KEYWORD_WITHOUT, Tokenizer::ROUTEX_KEY_NAME ],
+			[ Tokenizer::ROUTEX_KEYWORD_WITHOUT, Tokenizer::ROUTEX_KEY_CONTROLLER ],
+			[ Tokenizer::ROUTEX_KEYWORD_WITHOUT, Tokenizer::ROUTEX_KEY_MIDDLEWARE ],
+			[ Tokenizer::ROUTEX_KEYWORD_WITHOUT, NULL ],
+			[ Tokenizer::ROUTEX_KEY_PREFIX, Tokenizer::ROUTEX_KEY_NAME ],
+			[ Tokenizer::ROUTEX_KEY_PREFIX, Tokenizer::ROUTEX_KEY_CONTROLLER ],
+			[ Tokenizer::ROUTEX_KEY_PREFIX, Tokenizer::ROUTEX_KEY_MIDDLEWARE ],
+			[ Tokenizer::ROUTEX_KEY_PREFIX, NULL ],
+			[ Tokenizer::ROUTEX_KEY_NAME, Tokenizer::ROUTEX_KEY_PREFIX ],
+			[ Tokenizer::ROUTEX_KEY_NAME, Tokenizer::ROUTEX_KEY_CONTROLLER ],
+			[ Tokenizer::ROUTEX_KEY_NAME, Tokenizer::ROUTEX_KEY_MIDDLEWARE ],
+			[ Tokenizer::ROUTEX_KEY_NAME, NULL ],
+			[ Tokenizer::ROUTEX_KEY_CONTROLLER, Tokenizer::ROUTEX_KEY_PREFIX ],
+			[ Tokenizer::ROUTEX_KEY_CONTROLLER, Tokenizer::ROUTEX_KEY_NAME ],
+			[ Tokenizer::ROUTEX_KEY_CONTROLLER, Tokenizer::ROUTEX_KEY_MIDDLEWARE ],
+			[ Tokenizer::ROUTEX_KEY_CONTROLLER, NULL ],
+			[ Tokenizer::ROUTEX_KEY_MIDDLEWARE, Tokenizer::ROUTEX_KEY_NAME ],
+			[ Tokenizer::ROUTEX_KEY_MIDDLEWARE, Tokenizer::ROUTEX_KEY_CONTROLLER ],
+			[ Tokenizer::ROUTEX_KEY_MIDDLEWARE, Tokenizer::ROUTEX_KEY_PREFIX ],
+			[ Tokenizer::ROUTEX_KEY_MIDDLEWARE, NULL ],
+		],
+	];
+
+	public static function breakInLines(array $tokens)
 	{
 		$emptyLine = [['token' => NULL, 'token_name' => 'EMPTY_LINE']];
 		$context = null;
@@ -64,7 +121,87 @@ class SyntaxAnalyzer
 		}
 		//
 		return $lines;
-	} 
+	}
+
+	public static function syntaxCheck(array $lines, array &$errors = null)
+	{
+		$errors = [];
+		//
+		foreach ($lines as $index => $line_tokens) {
+			if (! self::syntaxCheckLine($line_tokens, $error)) {
+				$linenumber = 1 + $index;
+				$line = "line {$linenumber}: " . self::lineFromTokens($line_tokens);
+				$errors[] = compact('line','error');
+			}
+		}
+		//
+		return empty($errors);
+	}
+
+	protected static function syntaxCheckLine(array $line, &$error = null)
+	{
+		$last = $current = $context = null;
+		//
+		foreach ($line as $position => $token) {
+			list($last, $current) = array($current, $token);
+			//
+			if (is_null($last)) {
+				$context = $token['token_id'] ?? 0;
+				continue;
+			}
+			//
+			if (! self::isValidSequence($last, $current, $context, $expected)) {
+				$error = sprintf(
+					'After (%s) was expected (%s) but found (%s).',
+					$last['token'],
+					implode(' or ', $expected),
+					$current['token']
+				);
+				//
+				return false;
+			}
+		}
+		//
+		return true;
+	}
+
+	protected static function lineFromTokens(array $tokens)
+	{
+		$line = array_map(function($token) {
+			return $token['token'] ?? $token;
+		}, $tokens);
+		//
+		return implode(' ', $line);
+	}
+
+	protected static function isValidSequence(
+		$lastToken, $currentToken, $context, array &$expected = null
+	) {
+		$partial = array($lastToken['token_id'], $currentToken['token_id']);
+		$sequences = self::ROUTEX_VALID_SEQUENCES[$context] ?? [];
+		//
+		if (empty($sequences)) {
+			return false;
+		}
+		//
+		$expected = [];
+		//
+		foreach ($sequences as $p => $sequence) {
+			if ($partial[0] === $sequence[0]) {
+				if ($partial === $sequence) {
+					$expected = [];
+					return true;
+				} else {
+					if (!empty($sequence[1])) {
+						$expected[] = Tokenizer::ROUTEX_TOKEN_NAMES[$sequence[1]]
+							?? Tokenizer::ROUTEX_TOKEN_NAMES[Tokenizer::ROUTEX_UNKNOWN];
+					}
+				}
+			}
+		}
+		//
+		return false;
+	}
 
 }
 
